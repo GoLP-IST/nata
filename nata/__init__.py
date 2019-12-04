ef0e10b5f45c63a9572af8f6675d7986786bad33
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2019-12-04 14:16:59 +0000

    added package init

---
+++ nata/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.0.1.dev0"
+
+__title__ = "nata"
+
+__description__ = "Post-processing and visualization for PIC codes"
+__url__ = "NOT DEFINED YET"  # TODO: add url here
+__uri__ = __url__
+__doc__ = __description__ + "<" + __uri__ + ">"
+
+__author__ = "Anton Helm, Fábio Cruz"
+__email__ = "anton.helm@tecnico.ulisboa.pt, fabio.cruz@tecnico.ulisboa.pt"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2019 Anton Helm and Fábio Cruz"
