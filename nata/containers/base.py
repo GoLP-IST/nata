64e3d6c707e9623a8491b3299896ea4d2715a47a
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-02-23 09:08:09 +0000

    modification to BaseDataset to support new structure

--- nata/containers/base.py
+++ nata/containers/base.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 from abc import ABC
-from pathlib import Path
 from typing import Any
-from typing import Optional
+from typing import List
 from typing import Set
 
 import attr
-from attr import validators
+import numpy as np
 
-from nata.containers import location_exist
+from nata.utils.exceptions import NataInvalidContainer
 
 
 def register_backend(container):
@@ -22,14 +21,29 @@ def register_backend(container):
     return add_to_backend
 
 
+# TODO: specify data type that it is a list of np.ndarrays and backends with
+#       read_data method
+def convert_unstructured_data_to_array(
+    data: List[Any], dtype, indices, fields=None
+):
+    # read out arrays if they are not in the list
+    for i, elem in enumerate(data):
+        if not isinstance(elem, np.ndarray):
+            if fields is None:
+                data[i] = elem.get_data(indices)
+            else:
+                data[i] = elem.get_data(indices, fields)
+
+    # `copy=False` should never be fulfilled and a copy should be done
+    #              we use it here just in case if we can avoid a copy, the rest
+    #              is done by numpy as it should check internally
+    return np.squeeze(np.array(data, dtype=dtype, copy=False))
+
+
 @attr.s(init=False)
 class BaseDataset(ABC):
     _backends: Set[Any] = set()
     appendable = False
-
-    location: Optional[Path] = attr.ib(
-        validator=validators.optional(location_exist), eq=False, order=False
-    )
 
     @classmethod
     def register_plugin(cls, plugin_name, plugin):
@@ -38,3 +52,26 @@ class BaseDataset(ABC):
     @classmethod
     def add_backend(cls, backend):
         cls._backends.add(backend)
+
+    def _convert_to_backend(self, obj):
+        for backend in self._backends:
+            if backend.is_valid_backend(obj):
+                return backend(obj)
+
+        raise NataInvalidContainer(
+            f"Unable to find proper backend for {type(obj)}"
+        )
+
+    def _check_appendability(self, other: "BaseDataset"):
+        if not self.appendable:
+            raise TypeError(f"'{self.__class__}' is not appendable")
+
+        if not isinstance(other, self.__class__):
+            raise TypeError(
+                f"Can not append '{type(other)}' to '{self.__class__}'"
+            )
+
+        if self != other:
+            raise ValueError(
+                f"Can not append different '{self.__class__.__name__}'"
+            )
