b3f09e2dd0cbce227e512c33ebba0bcd87bd031f
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-02-27 14:51:14 +0000

    changed order of imports and remove unnecessary

--- nata/backends/__init__.py
+++ nata/backends/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# import different backends - each backend should register in a container
-import nata.backends.osiris
-
 from .grid import GridBackend
-from .particles import BaseParticles
+from .particles import ParticleBackend
+
+# import different backends - each backend should register in a container
+from .osiris import *  # noqa isort:skip
