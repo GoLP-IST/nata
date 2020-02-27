4073af2f85a3aefb78109d2e72db48bb052a7105
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-02-27 15:13:07 +0000

    trying isort ordering

--- nata/backends/__init__.py
+++ nata/backends/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from .grid import GridBackend
-from .particles import ParticleBackend
 
-# import different backends - each backend should register in a container
-from .osiris import *  # noqa isort:skip
+from .grid import GridBackend
+from .osiris import *
+from .particles import ParticleBackend
