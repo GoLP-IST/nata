7eb40c3ca9e3a0e640e2a7c92d012c1625e54b5c
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-02-28 09:12:14 +0000

    unification of dataset types for typing

--- nata/containers/__init__.py
+++ nata/containers/__init__.py
@@ -1,7 +1,12 @@
-from .helpers import location_exist
+# -*- coding: utf-8 -*-
+# flake8: noqa: E402
+from typing import Union
+
+
 from .base import register_backend
-from .base import BaseDataset
 from .grid import GridDataset
 from .particles import ParticleDataset
-from .collection import DatasetCollection
 
+DatasetTypes = Union[GridDataset, ParticleDataset]
+
+from .collection import DatasetCollection
