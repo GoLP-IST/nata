c7262a00956cc538ee35d806ef238a2e39a87aaf
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-03-13 17:57:27 +0000

    updates interface for ParticleDatasets

--- nata/containers.py
+++ nata/containers.py
@@ -525,90 +525,154 @@ ParticleBackendBased = TypeVar(
 )
 
 
-@attr.s(init=False, eq=False)
+def _generate_quantities_list(data: List[Union[ParticleBackend, np.ndarray]]):
+    quantity_names = tuple()
+    quantity_labels = tuple()
+    quantity_units = tuple()
+    quantity_dtype = None
+
+    number_particles = []
+
+    for d in data:
+        if not hasattr(d, "quantities"):
+            continue
+
+        names = tuple(d.quantities)
+        labels = tuple(d.quantity_labels)
+        units = tuple(d.quantity_units)
+        dtype = d.dtype
+
+        number_particles.append(d.num_particles)
+
+        if (
+            any(
+                tuple() == v
+                for v in (quantity_names, quantity_labels, quantity_units,)
+            )
+            and quantity_dtype is None
+        ):
+            quantity_names = names
+            quantity_labels = labels
+            quantity_units = units
+            quantity_dtype = dtype
+        else:
+            if (names, labels, units, dtype) != (
+                quantity_names,
+                quantity_labels,
+                quantity_units,
+                quantity_dtype,
+            ):
+                raise ValueError("Mismatch between quantity props in data")
+
+    quantities = []
+
+    for name, label, unit in zip(
+        quantity_names, quantity_labels, quantity_units
+    ):
+        quantities.append(
+            ParticleQuantity(
+                name=name,
+                label=label,
+                unit=unit,
+                data=data,
+                len=number_particles,
+                dtype=quantity_dtype[name],
+            )
+        )
+
+    return quantities
+
+
+@attr.s(eq=False)
 class ParticleDataset(BaseDataset):
     """Container class storing particle datasets"""
 
     _backends: Set[ParticleBackend] = set((ParticleArray,))
-    backend: Optional[str] = attr.ib(validator=optional(subdtype_of(np.str_)))
     appendable = True
 
-    name: str = attr.ib(validator=subdtype_of(np.str_))
-    dtype: np.dtype = attr.ib(validator=instance_of(np.dtype))
-
-    iteration: IterationAxis = attr.ib(validator=instance_of(IterationAxis))
-    time: TimeAxis = attr.ib(validator=instance_of(TimeAxis))
-    num_particles: UnnamedAxis = attr.ib(validator=instance_of(UnnamedAxis))
-
-    quantities: np.ndarray = attr.ib(
-        validator=deep_iterable(
-            member_validator=subdtype_of(np.str_),
-            iterable_validator=instance_of(np.ndarray),
-        ),
-        eq=False,
+    _data: np.ndarray = attr.ib(
+        converter=_asanyarray_converter, repr=False, eq=False
     )
-    quantity_labels: np.ndarray = attr.ib(
-        validator=deep_iterable(
-            member_validator=subdtype_of(np.str_),
-            iterable_validator=instance_of(np.ndarray),
-        ),
-        eq=False,
+    backend: Optional[str] = attr.ib(
+        default=None, validator=optional(subdtype_of(np.str_))
     )
-    quantity_units: np.ndarray = attr.ib(
-        validator=deep_iterable(
-            member_validator=subdtype_of(np.str_),
-            iterable_validator=instance_of(np.ndarray),
+
+    name: str = attr.ib(default=None, validator=optional(subdtype_of(np.str_)))
+
+    iteration: IterationAxis = attr.ib(
+        default=None, validator=optional(instance_of(IterationAxis))
+    )
+    time: TimeAxis = attr.ib(
+        default=None, validator=optional(instance_of(TimeAxis))
+    )
+
+    num_particles: UnnamedAxis = attr.ib(
+        default=None, validator=optional(instance_of(UnnamedAxis))
+    )
+    quantities: List[Optional[ParticleQuantity]] = attr.ib(
+        default=None,
+        validator=optional(
+            deep_iterable(
+                member_validator=optional(instance_of(ParticleQuantity)),
+                iterable_validator=instance_of(list),
+            )
         ),
-        eq=False,
     )
 
     def info(self, full: bool = False):  # pragma: no cover
         return self.__repr__()
 
-    def __init__(
-        self,
-        particles: Optional[ParticleBackendBased],
-        **kwargs: Dict[str, Any],
-    ):
-        if particles is None:
-            self._init_from_kwargs(**kwargs)
-        else:
-            self._init_from_backend(particles)
+    def __attrs_post_init__(self):
+        for i, d in enumerate(self._data):
+            if not isinstance(d, (np.ndarray, ParticleBackend)):
+                self._data[i] = self._convert_to_backend(d)
+
+        self.backend = (
+            self.backend
+            if self.backend is not None
+            else _deduct_prop_from_data(self._data, "name")
+        )
+
+        self.name = (
+            self.name
+            if self.name is not None
+            else _deduct_prop_from_data(self._data, "dataset_name")
+        )
+
+        self.iteration = (
+            self.iteration
+            if self.iteration is not None
+            else IterationAxis(
+                data=_deduct_prop_from_data(self._data, "iteration")
+            )
+        )
+        self.time = (
+            self.time
+            if self.time is not None
+            else TimeAxis(
+                data=_deduct_prop_from_data(self._data, "time_step"),
+                unit=_deduct_prop_from_data(self._data, "time_unit", ""),
+            )
+        )
+
+        self.num_particles = (
+            self.num_particles
+            if self.num_particles is not None
+            else UnnamedAxis(
+                data=_deduct_prop_from_data(self._data, "num_particles")
+            )
+        )
+
+        self.quantities = (
+            self.quantities
+            if self.quantities is not None
+            else _generate_quantities_list(self._data)
+        )
+
+        for quant in self.quantities:
+            setattr(self, quant.name, quant)
 
         attr.validate(self)
-
-    def _init_from_kwargs(self, **kwargs: Dict[str, Any]):
-        raise NotImplementedError
-
-    def _init_from_backend(self, particles: ParticleBackendBased):
-        if not isinstance(particles, ParticleBackend):
-            particles = self._convert_to_backend(particles)
-
-        self.backend = particles.name
-
-        self.name = particles.dataset_name
-        self.dtype = particles.dtype
-
-        self.iteration = IterationAxis(data=particles.iteration)
-        self.time = TimeAxis(data=particles.time_step, unit=particles.time_unit)
-        self.num_particles = UnnamedAxis(data=particles.num_particles)
-
-        self.quantities = np.asarray(particles.quantities)
-        self.quantity_labels = np.asarray(particles.quantity_labels)
-        self.quantity_units = np.asarray(particles.quantity_units)
-
-        for quantity, label, unit in zip(
-            self.quantities, self.quantity_labels, self.quantity_units
-        ):
-            q = ParticleQuantity(
-                data=[particles],
-                len=[particles.num_particles],
-                dtype=self.dtype[quantity],
-                name=quantity,
-                label=label,
-                unit=unit,
-            )
-            setattr(self, quantity, q)
 
     def _check_dataset_equality(self, other: Union["ParticleDataset", Any]):
         if not isinstance(other, self.__class__):
@@ -619,18 +683,17 @@ class ParticleDataset(BaseDataset):
 
         return True
 
-    def append(self, other: Union["ParticleDataset", Any]):
+    def append(self, other: "ParticleDataset"):
         self._check_appendability(other)
+
+        self._data = np.array(
+            [d for d in self._data] + [d for d in other._data]
+        )
 
         self.iteration.append(other.iteration)
         self.time.append(other.time)
-        self.num_particles.append(other.num_particles)
-
-        for q in self.quantities:
-            getattr(self, q).append(getattr(other, q))
-
-    def __len__(self):
-        return len(self.iteration)
+        for self_quant, other_quant in zip(self.quantities, other.quantities):
+            self_quant.append(other_quant)
 
 
 DatasetTypes = TypeVar("DatasetTypes", GridDataset, ParticleDataset)
