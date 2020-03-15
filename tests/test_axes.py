239642a4785af610fc8771053fd8cbe2c8138679
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-03-15 20:53:07 +0000

    adds tets for Axis, IterationAxis and TimeAxis

--- tests/test_axes.py
+++ tests/test_axes.py
@@ -3,7 +3,11 @@ import numpy as np
 import pytest
 from hypothesis import given
 from hypothesis.strategies import one_of
+from hypothesis.strategies import text
 
+from nata.axes import Axis
+from nata.axes import IterationAxis
+from nata.axes import TimeAxis
 from nata.axes import UnnamedAxis
 from nata.utils.formatting import array_format
 
@@ -95,3 +99,48 @@ def test_UnnamedAxis_append_1d_arrays(arr1, arr2):
     assert axis.ndim == arr.ndim
     assert axis.dtype == arr.dtype
     assert len(axis) == len(arr)
+
+
+@given(one_of(number_or_none(), anyarray(max_dims=1)), text(), text(), text())
+def test_Axis_init(data, name, label, unit):
+    axis = Axis(data, name, label, unit)
+
+    assert axis.name == name
+    assert axis.label == label
+    assert axis.unit == unit
+
+
+@given(one_of(number_or_none(), anyarray(max_dims=1)), text(), text(), text())
+def test_IterationAxis_init(data, name, label, unit):
+    axis = IterationAxis(data, name, label, unit)
+
+    assert axis.name == name
+    assert axis.label == label
+    assert axis.unit == unit
+
+
+@given(one_of(number_or_none(), anyarray(max_dims=1)))
+def test_IterationAxis_default(data):
+    axis = IterationAxis(data)
+
+    assert axis.name == "iteration"
+    assert axis.label == "iteration"
+    assert axis.unit == ""
+
+
+@given(one_of(number_or_none(), anyarray(max_dims=1)), text(), text(), text())
+def test_TimeAxis_init(data, name, label, unit):
+    axis = TimeAxis(data, name, label, unit)
+
+    assert axis.name == name
+    assert axis.label == label
+    assert axis.unit == unit
+
+
+@given(one_of(number_or_none(), anyarray(max_dims=1)))
+def test_TimeAxis_default(data):
+    axis = TimeAxis(data)
+
+    assert axis.name == "time"
+    assert axis.label == "time"
+    assert axis.unit == ""
