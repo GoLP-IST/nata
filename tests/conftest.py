d0c7732ea9897b0082416b04600566d8976c2965
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-03-15 19:03:03 +0000

    removed unused import from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
+# -*- coding: utf-8 -*-
 from contextlib import contextmanager
-
-import pytest
 
 
 def pytest_configure(config):
     config.addinivalue_line(
         "markers", "wip: work-in-progress marker to run currently "
     )
+
 
 @contextmanager
 def does_not_raise():
