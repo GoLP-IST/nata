874f93276ca2c4f3ebdc275455d94533868ab140
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-03-15 23:10:42 +0000

    adds profiles for hypothesis

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 from contextlib import contextmanager
+
+from hypothesis import Verbosity
+from hypothesis import settings
 
 
 def pytest_configure(config):
@@ -11,3 +15,11 @@ def pytest_configure(config):
 @contextmanager
 def does_not_raise():
     yield
+
+
+# taken from:
+# https://hypothesis.readthedocs.io/en/latest/settings.html#settings-profiles
+settings.register_profile("ci", max_examples=1000)
+settings.register_profile("dev", max_examples=10)
+settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
+settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
