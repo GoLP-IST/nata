e703a7f6b54519b651de8b281007e1d6dbd75f13
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-04-15 15:50:32 +0100

    changes to hypothesis settings and new verbose profile

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,5 +21,10 @@ def does_not_raise():
 # https://hypothesis.readthedocs.io/en/latest/settings.html#settings-profiles
 settings.register_profile("ci", max_examples=1000)
 settings.register_profile("dev", max_examples=10)
-settings.register_profile("debug", max_examples=10, verbosity=Verbosity.verbose)
+settings.register_profile(
+    "debug", max_examples=10, report_multiple_bugs=False,
+)
+settings.register_profile(
+    "verbose", max_examples=10, verbosity=Verbosity.verbose,
+)
 settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
