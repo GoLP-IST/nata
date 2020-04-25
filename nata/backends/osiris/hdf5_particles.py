16845981c187a71ca0635162b5196edd51a5e71e
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-04-26 00:38:04 +0100

    added back particles backend for osiris hdf5

--- nata/backends/osiris/hdf5_particles.py
+++ nata/backends/osiris/hdf5_particles.py
@@ -1,38 +1,45 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
+from typing import List
+from typing import Optional
 from typing import Union
 
 import h5py as h5
 import numpy as np
 
 from nata.containers import ParticleDataset
+from nata.types import ParticleBackendType
 from nata.utils.container_tools import register_backend
-
-from ..particles import ParticleBackend
 
 
 @register_backend(ParticleDataset)
-class Osiris_Hdf5_ParticleFile(ParticleBackend):
+class Osiris_Hdf5_ParticleFile(ParticleBackendType):
     name = "osiris_hdf5_particles"
+    location: Optional[Path] = None
+
+    def __init__(self, location=Union[str, Path]) -> None:
+        self.location = (
+            location if isinstance(location, Path) else Path(location)
+        )
 
     @staticmethod
-    def is_valid_backend(file_path: Union[Path, str]) -> bool:
-        if isinstance(file_path, str):
-            file_path = Path(file_path)
+    def is_valid_backend(path: Union[Path, str]) -> bool:
+        if isinstance(path, str):
+            path = Path(path)
 
-        if not isinstance(file_path, Path):
+        if not isinstance(path, Path):
             return False
 
-        if not file_path.is_file():
+        if not path.is_file():
             return False
 
-        if not file_path.suffix == ".h5":
+        if not path.suffix == ".h5":
             return False
 
-        if not h5.is_hdf5(file_path):
+        if not h5.is_hdf5(path):
             return False
 
-        with h5.File(file_path, mode="r") as f:
+        with h5.File(path, mode="r") as f:
             if ("TYPE" in f.attrs) and ("LABELS" not in f.attrs):
                 type_ = f.attrs["TYPE"].astype(str)[0]
 
@@ -44,41 +51,23 @@ class Osiris_Hdf5_ParticleFile(ParticleBackend):
     @property
     def dataset_name(self) -> str:
         with h5.File(self.location, mode="r") as fp:
-            return fp.attrs["NAME"].astype(str)[0]
+            dataset_name = fp.attrs["NAME"].astype(str)[0]
+        return dataset_name
 
     @property
     def num_particles(self) -> int:
         with h5.File(self.location, mode="r") as fp:
-            return fp["q"].shape[0]
+            num_particles: int = fp["q"].shape[0]
+        return num_particles
 
-    @property
-    def has_tags(self):
-        with h5.File(self.location, mode="r") as fp:
-            return "tag" in fp
-
-    @property
-    def tags(self):
-        if not self.has_tags:
-            raise AttributeError(
-                f'The file "{self.location}" does not include tags!'
-            )
-
-        with h5.File(self.location, mode="r") as fp:
-            tag_dset = fp["tag"]
-            tags = np.zeros(tag_dset.shape, dtype=tag_dset.dtype)
-            tag_dset.read_direct(tags)
-
-        return set((node, tag) for node, tag in tags)
-
-    # TODO: allow indexing
-    def get_data(self, indexing=None, fields=None):
+    def get_data(self, indexing=None, fields=None) -> np.ndarray:
         with h5.File(self.location, mode="r") as fp:
             if fields is None:
                 # create a structured array
                 dset = np.empty(self.num_particles, dtype=self.dtype)
 
                 # fill the array
-                for quant in self.quantities:
+                for quant in self.quantity_names:
                     dset[quant] = fp[quant]
             else:
                 if indexing is None:
@@ -89,7 +78,7 @@ class Osiris_Hdf5_ParticleFile(ParticleBackend):
         return dset
 
     @property
-    def quantities(self):
+    def quantity_names(self) -> List[str]:
         quantities = []
 
         with h5.File(self.location, mode="r") as fp:
@@ -99,44 +88,48 @@ class Osiris_Hdf5_ParticleFile(ParticleBackend):
                 if isinstance(item, h5.Dataset):
                     quantities.append(key)
 
-        return np.array(quantities)
+        return quantities
 
     @property
-    def quantity_labels(self):
+    def quantity_labels(self) -> List[str]:
         names = []
         with h5.File(self.location, mode="r") as fp:
-            for quant in self.quantities:
-                names.append(fp[quant].attrs["LONG_NAME"].astype(str)[0])
+            for quant in self.quantity_names:
+                name: str = fp[quant].attrs["LONG_NAME"].astype(str)[0]
+                names.append(name)
         return names
 
     @property
-    def quantity_units(self):
+    def quantity_units(self) -> List[str]:
         units = []
         with h5.File(self.location, mode="r") as fp:
-            for quant in self.quantities:
+            for quant in self.quantity_names:
                 units.append(fp[quant].attrs["UNITS"].astype(str)[0])
 
         return units
 
     @property
-    def dtype(self):
+    def dtype(self) -> np.dtype:
         fields = []
         with h5.File(self.location, mode="r") as fp:
-            for quant in self.quantities:
+            for quant in self.quantity_names:
                 fields.append((quant, fp[quant].dtype))
         return np.dtype(fields)
 
     @property
-    def iteration(self):
+    def iteration(self) -> int:
         with h5.File(self.location, mode="r") as fp:
-            return fp.attrs["ITER"][0]
+            iteration = fp.attrs["ITER"][0]
+        return iteration
 
     @property
-    def time_step(self):
+    def time_step(self) -> float:
         with h5.File(self.location, mode="r") as fp:
-            return fp.attrs["TIME"][0]
+            time_step = fp.attrs["TIME"][0]
+        return time_step
 
     @property
-    def time_unit(self):
+    def time_unit(self) -> str:
         with h5.File(self.location, mode="r") as fp:
-            return fp.attrs["TIME UNITS"].astype(str)[0]
+            time_unit = fp.attrs["TIME UNITS"].astype(str)[0]
+        return time_unit
