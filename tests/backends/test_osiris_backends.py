5213d1d03ac7747cef2ea313a424852f59ebd983
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-04-26 00:42:20 +0100

    added test

--- tests/backends/test_osiris_backends.py
+++ tests/backends/test_osiris_backends.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from nata.backends.osiris.hdf5_grid import Osiris_Hdf5_GridFile
 from nata.backends.osiris.hdf5_grid_dev import Osiris_Dev_Hdf5_GridFile
+from nata.backends.osiris.hdf5_particles import Osiris_Hdf5_ParticleFile
 from nata.backends.osiris.zdf_grid import Osiris_zdf_GridFile
 from nata.containers import GridDataset
+from nata.containers import ParticleDataset
 from nata.types import GridBackendType
+from nata.types import ParticleBackendType
 
 
 def test_Osiris_Hdf5_GridFile_isinstance_GridBackendType():
@@ -18,10 +23,21 @@ def test_Osiris_zdf_GridFile_isinstance_GridBackendType():
     assert isinstance(Osiris_zdf_GridFile, GridBackendType)
 
 
-def test_backends_are_registered():
+@pytest.mark.wip
+def test_Osiris_Hdf5_ParticleFile_ParticleBackendType():
+    assert isinstance(Osiris_Hdf5_ParticleFile, ParticleBackendType)
+
+
+def test_GridDatasets_backends_are_registered():
     backends = GridDataset.get_backends()
 
-    # avoids name collisions
     assert backends[Osiris_Hdf5_GridFile.name] is Osiris_Hdf5_GridFile
     assert backends[Osiris_Dev_Hdf5_GridFile.name] is Osiris_Dev_Hdf5_GridFile
     assert backends[Osiris_zdf_GridFile.name] is Osiris_zdf_GridFile
+
+
+@pytest.mark.wip
+def test_ParticleDatasets_backends_are_registered():
+    backends = ParticleDataset.get_backends()
+
+    assert backends[Osiris_Hdf5_ParticleFile.name] is Osiris_Hdf5_ParticleFile
