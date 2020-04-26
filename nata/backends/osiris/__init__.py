97136f16001ffb13c5f74d9b5e1c6125f3b56cea
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-04-26 15:50:03 +0100

    moved zdf interface to new file

--- nata/backends/osiris/__init__.py
+++ nata/backends/osiris/__init__.py
@@ -2,9 +2,9 @@
 # grid backends
 from .hdf5 import Osiris_Hdf5_GridFile
 from .hdf5 import Osiris_Dev_Hdf5_GridFile
-from .zdf_grid import Osiris_zdf_GridFile
+from .zdf import Osiris_zdf_GridFile
 
 # particle backends
 from .hdf5 import Osiris_Hdf5_ParticleFile
 from .hdf5 import Osiris_Dev_Hdf5_ParticleFile
-from .zdf_particles import Osiris_zdf_ParticleFile
+from .zdf import Osiris_zdf_ParticleFile
