57858002e63819297bc81a1517c0d048f3f2c3ec
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-04-28 11:48:06 +0100

    quickfix for cached_property causing issues

--- nata/utils/cached_property.py
+++ nata/utils/cached_property.py
@@ -5,12 +5,15 @@ import sys
 if sys.version_info >= (3, 8):
     from functools import cached_property
 else:
+    cached_property = property
 
-    class cached_property:
-        def __init__(self, func):
-            self._func = func
+    # something is wrong in the implementation
+    # class cached_property:
+    #     def __init__(self, func):
+    #         self._func = func
 
-        def __get__(self, instance, owner=None):
-            assert instance is not None
-            ret = instance.__dict__[self._func.__name__] = self._func(instance)
-            return ret
+    #     def __get__(self, instance, owner=None):
+    #         assert instance is not None
+    #         ret = \
+    #           instance.__dict__[self._func.__name__] = self._func(instance)
+    #         return ret
