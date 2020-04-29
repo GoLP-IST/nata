1e688e30f22cab77e2a34499efb5db45415fa9d6
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2020-04-29 17:49:52 +0100

    adds filter plugin doc

--- nata/plugins/particles/filter.py
+++ nata/plugins/particles/filter.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from typing import List
-from typing import Optional
 
 from nata.containers import ParticleDataset
 from nata.plugins.register import register_container_plugin
@@ -8,8 +7,34 @@ from nata.plugins.register import register_container_plugin
 
 @register_container_plugin(ParticleDataset, name="filter")
 def filter_particle_dataset(
-    dataset: ParticleDataset, quantities: Optional[List[str]] = None
+    dataset: ParticleDataset, quantities: List[str]
 ) -> ParticleDataset:
+    """Filters a :class:`nata.containers.ParticleDataset` according to a\
+       selection of quantities.
+
+        Parameters
+        ----------
+        quantities: ``list``
+            List of quantities to be filtered, ordered by the way they should be
+            sorted in the returned dataset.
+
+        Returns
+        ------
+        :class:`nata.containers.ParticleDataset`:
+            Filtered dataset with only the quantities selected in
+            ``quantities``.
+
+        Examples
+        --------
+        The filter plugin is used to get dataset with only a selection, say
+        ``'x1'`` and ``'x2'``, of its quantities.
+
+        >>> from nata.containers import ParticleDataset
+        >>> ds = ParticleDataset("path/to"file")
+        >>> ds_flt = ds.filter(quantities=["x1","p1"])
+
+    """
+
     if quantities is not None:
         quants = {}
         for quant in quantities:
@@ -18,10 +43,12 @@ def filter_particle_dataset(
                 if quant in dataset.quantities.keys()
                 else None
             )
+    else:
+        raise ValueError("")
 
     return ParticleDataset(
         iteration=dataset.axes["iteration"],
         time=dataset.axes["time"],
         name=dataset.name,
-        quantities=quants if quantities is not None else dataset.quantities,
+        quantities=quants,
     )
