915ce0efe61541482f1c4d1aaba3e15520d8bb6f
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2020-08-07 13:01:13 +0100

    adds slicing to particle filter

--- nata/plugins/particles/filter.py
+++ nata/plugins/particles/filter.py
@@ -5,6 +5,7 @@ from typing import List
 import numpy as np
 
 from nata.containers import ParticleDataset
+from nata.containers import ParticleQuantity
 from nata.plugins.register import register_container_plugin
 
 
@@ -13,6 +14,7 @@ def filter_particle_dataset(
     dataset: ParticleDataset,
     mask: List[bool] = None,
     quantities: List[str] = None,
+    slicing: slice = None,
 ) -> ParticleDataset:
     """Filters a :class:`nata.containers.ParticleDataset` according to a\
        selection of quantities.
@@ -59,6 +61,22 @@ def filter_particle_dataset(
     if mask is not None:
         for name, quant in quants.items():
             quants[name].data[~mask] = np.ma.masked
+
+    if slicing is not None:
+        # allow particle index slicing only for now
+        if len(quant) > 1:
+            slicing = (slice(None), slicing)
+
+        quants = {
+            quant.name: ParticleQuantity(
+                data=quant.data[slicing],
+                name=quant.name,
+                label=quant.label,
+                unit=quant.unit,
+                dtype=quant.data.dtype,
+            )
+            for quant in quants.values()
+        }
 
     return ParticleDataset(
         iteration=dataset_c.axes["iteration"],
