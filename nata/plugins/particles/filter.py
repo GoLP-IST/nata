8fffe84985a2e1ad97e7a660944f07adeb2801a9
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2020-08-07 11:40:04 +0100

    Particle filter mask (#105)
    * create plot data with masked array
    * adds mask to particle filter plugin
    * uses np.ma.masked to mask arrays

--- nata/plugins/particles/filter.py
+++ nata/plugins/particles/filter.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
 from typing import List
+
+import numpy as np
 
 from nata.containers import ParticleDataset
 from nata.plugins.register import register_container_plugin
@@ -7,13 +10,20 @@ from nata.plugins.register import register_container_plugin
 
 @register_container_plugin(ParticleDataset, name="filter")
 def filter_particle_dataset(
-    dataset: ParticleDataset, quantities: List[str]
+    dataset: ParticleDataset,
+    mask: List[bool] = None,
+    quantities: List[str] = None,
 ) -> ParticleDataset:
     """Filters a :class:`nata.containers.ParticleDataset` according to a\
        selection of quantities.
 
         Parameters
         ----------
+        mask: ``np.ndarray``
+            Array of booleans indicating the particles to be filtered. Particles
+            with ``True`` (``False``) mask entries are selected (hidden). The
+            shape of ``mask`` must match that of each particle quantity.
+
         quantities: ``list``
             List of quantities to be filtered, ordered by the way they should be
             sorted in the returned dataset.
@@ -35,20 +45,24 @@ def filter_particle_dataset(
 
     """
 
+    dataset_c = deepcopy(dataset)
+
     if quantities is not None:
-        quants = {}
-        for quant in quantities:
-            quants[quant] = (
-                dataset.quantities[quant]
-                if quant in dataset.quantities.keys()
-                else None
-            )
+        quants = {
+            quant: dataset_c.quantities[quant]
+            for quant in quantities
+            if quant in dataset_c.quantities
+        }
     else:
-        raise ValueError("")
+        quants = dataset_c.quantities
+
+    if mask is not None:
+        for name, quant in quants.items():
+            quants[name].data[~mask] = np.ma.masked
 
     return ParticleDataset(
-        iteration=dataset.axes["iteration"],
-        time=dataset.axes["time"],
-        name=dataset.name,
+        iteration=dataset_c.axes["iteration"],
+        time=dataset_c.axes["time"],
+        name=dataset_c.name,
         quantities=quants,
     )
