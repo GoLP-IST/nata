f3de252505d405332d31b2ba5c4138c3ffb6c423
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2020-08-18 16:08:44 +0100

    adds validity of backend 'Osiris_Hdf5_GridFile'

--- tests/backends/test_osiris_backends.py
+++ tests/backends/test_osiris_backends.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import h5py as h5
+import numpy as np
+import pytest
+
 from nata.backends.osiris.hdf5 import Osiris_Dev_Hdf5_GridFile
 from nata.backends.osiris.hdf5 import Osiris_Dev_Hdf5_ParticleFile
 from nata.backends.osiris.hdf5 import Osiris_Hdf5_GridFile
@@ -52,3 +56,20 @@ def test_ParticleDatasets_backends_are_registered():
         is Osiris_Dev_Hdf5_ParticleFile
     )
     assert backends[Osiris_zdf_ParticleFile.name] is Osiris_zdf_ParticleFile
+
+
+@pytest.fixture(name="os_hdf5_grid_444_file")
+def _generate_valid_Osiris_Hdf5_GridFile(tmp_path):
+    file_ = tmp_path / "os_hdf5_grid_444_file.h5"
+
+    with h5.File(file_, mode="w") as fp:
+        fp.attrs["NAME"] = np.array([b"test ds"], dtype="|S256")
+        fp.attrs["TYPE"] = np.array([b"grid"], dtype="|S4")
+        data = np.asfortranarray(np.arange(24).reshape((2, 3, 4)))
+        fp.create_dataset("test ds", data=data)
+
+    return file_
+
+
+def test_Osiris_Hdf5_GridFile_check_is_valid_backend(os_hdf5_grid_444_file):
+    assert Osiris_Hdf5_GridFile.is_valid_backend(os_hdf5_grid_444_file) is True
