df859d8a12b0a33cf05cf7c7137f8f86708c6a26
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-04-10 21:34:37 +0000

    deactivate import in non-cleaned contaienrs

--- nata/containers/__init__.py
+++ nata/containers/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .GridDataset import GridDataset
-from .ParticleDataset import ParticleQuantity
-from .ParticleDataset import ParticleDataset
-from .DatasetCollection import DatasetCollection
+
+# from .ParticleDataset import ParticleQuantity
+# from .ParticleDataset import ParticleDataset
+# from .DatasetCollection import DatasetCollection
 from .axis import Axis
