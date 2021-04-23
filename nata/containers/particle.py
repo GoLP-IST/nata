52bbbdceb19601ec2f1d98ee2e00288e1fff3545
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-04-23 19:31:55 +0000

    create empty particle API

---
+++ nata/containers/particle.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+class Quantity:
+    pass
+
+
+class QuantityArray:
+    pass
+
+
+class Particle:
+    pass
+
+
+class ParticleArray:
+    pass
+
+
+class ParticleDataset:
+    pass
