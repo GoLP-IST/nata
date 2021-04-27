8b27f55bcbc3f9bc9dce76d3f0db679d556ec518
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2021-04-27 17:16:32 +0000

    adds streak docstring

--- nata/plugins/grids/streak.py
+++ nata/plugins/grids/streak.py
@@ -7,9 +7,33 @@ from nata.plugins.register import register_container_plugin
 
 
 @register_container_plugin(GridDataset, name="streak")
-def slice_grid_array(
+def streak_grid_array(
     grid: GridDataset,
 ) -> GridArray:
+    """Converts a `GridDataset` to a `GridArray`. Only `GridDataset` with axes
+    that do not change over time are supported.
+
+    Returns
+    ------
+    :class:`nata.containers.GridArray`:
+        Streak of ``grid``.
+
+    Examples
+    --------
+    Convert a one-dimensional dataset with time dependence to a two-dimensional
+    array.
+
+    >>> from nata.containers import GridDataset
+    >>> import numpy as np
+    >>> data = np.arange(5*7).reshape((5, 7))
+    >>> grid = GridDataset.from_array(data)
+    >>> stk_grid = grid.streak()
+    >>> stk_grid.shape
+    (5, 7)
+    >>> [axis.shape for axis in stk_grid.axes]
+    [(5,), (7,)]
+
+    """
 
     if grid.ndim < 2:
         raise ValueError("streak is not available for 0 dimensional GridDatasets")
