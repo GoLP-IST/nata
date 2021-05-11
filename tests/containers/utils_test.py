7df6afdc573a269b72eca917b909e8595a6bc6b2
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-05-11 20:48:37 +0000

    add convience functions to 'to_dask' and 'to_numpy'

--- tests/containers/utils_test.py
+++ tests/containers/utils_test.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import dask.array as da
 import numpy as np
+import pytest
 from numpy.lib import recfunctions
 
+from nata.containers.core import HasNumpyInterface
+from nata.containers.utils import to_dask
+from nata.containers.utils import to_numpy
 from nata.containers.utils import unstructured_to_structured
 
 
@@ -18,3 +22,35 @@ def test_unstructured_to_structured():
 
     assert isinstance(converted_arr, da.Array)
     np.testing.assert_array_equal(converted_arr, expected)
+
+
+def test_to_dask():
+    class ClassWithNumpyInterface(HasNumpyInterface):
+        pass
+
+    class ClassWithoutNumpyInterface:
+        pass
+
+    assert isinstance(to_dask(ClassWithNumpyInterface(da.arange(10))), da.Array)
+
+    with pytest.raises(
+        TypeError,
+        match=r"requires object of type '.*HasNumpyInterface.*'",
+    ):
+        to_dask(ClassWithoutNumpyInterface())
+
+
+def test_to_numpy():
+    class ClassWithNumpyInterface(HasNumpyInterface):
+        pass
+
+    class ClassWithoutNumpyInterface:
+        pass
+
+    assert isinstance(to_numpy(ClassWithNumpyInterface(da.arange(10))), np.ndarray)
+
+    with pytest.raises(
+        TypeError,
+        match=r"requires object of type '.*HasNumpyInterface.*'",
+    ):
+        to_dask(ClassWithoutNumpyInterface())
