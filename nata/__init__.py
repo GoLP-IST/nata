4c30ca525da05e3d1bf4e1d032c191cbcec7f2a5
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-05-13 16:13:34 +0000

    add locations to example data

--- nata/__init__.py
+++ nata/__init__.py
@@ -4,3 +4,12 @@
 # from nata.comfort import activate_logging
 from nata.containers import *
 from nata.plugins import *
+
+from collections import namedtuple
+from pathlib import Path
+
+_Examples = namedtuple("Examples", ["grids", "particles"])
+examples = _Examples(
+    Path(__file__).parent / ".." / "examples" / "data" / "grids",
+    Path(__file__).parent / ".." / "examples" / "data" / "particles",
+)
