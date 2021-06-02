01d1aabbf4cfe1043948ce9fc7c60b3b7ab95460
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2021-06-02 12:50:30 +0000

    Add docstring to slice plugin Fixes #166

---
+++ docs/plugins/grids.md
@@ -0,0 +1,15 @@
+# Grid plugins
+
+## List of plugins
+
+Plugin  | Description       | `GridArray` | `GridDataset`
+--------|-------------------|-------------|--------------
+Slice   | This slices grids | `.slice`    | .slice`
+
+## `GridArray`
+
+### ::: nata.plugins.grids.slice.slice_grid_array
+
+## `GridDataset`
+
+### ::: nata.plugins.grids.slice.slice_grid_dataset
--- nata/plugins/grids/slice.py
+++ nata/plugins/grids/slice.py
@@ -32,35 +32,33 @@ def slice_grid_array(
     constant: Union[str, int],
     value: float,
 ) -> GridArray:
-    """Takes a slice of a `GridArray` at a constant value of a given axis.
+    """
+    Takes a slice of a `GridArray` at a constant value of a given axis.
 
-    Parameters
-    ----------
-    constant: ``str`` or ``int``
-        Name or index that defines the axis taken to be constant in the slice.
-    comp: ``float``
-        Value of the axis at which the slice is taken.
+    Arguments:
+        constant:
+            Name or index that defines the axis taken to be constant in the slice.
+        value:
+            Value of the axis at which the slice is taken.
 
-    Returns
-    ------
-    :class:`nata.containers.GridArray`:
+    Returns:
         Slice of ``grid``.
 
-    Examples
-    --------
-    Obtain a slice of a two-dimensional array.
+    Examples:
+        Obtain a slice of a two-dimensional array.
 
-    >>> from nata.containers import GridArray
-    >>> from nata.containers import Axis
-    >>> import numpy as np
-    >>> x = np.arange(5)
-    >>> data = np.arange(25).reshape((5, 5))
-    >>> grid = GridArray.from_array(data, axes=[Axis(x), Axis(x)])
-    >>> grid.slice(constant=0, value=1).to_numpy()
-    array([5, 6, 7, 8, 9]) # the second column
-    >>> grid.slice(constant=1, value=1).to_numpy()
-    array([ 1,  6, 11, 16, 21]) # the second row
-
+        ```pycon
+        >>> from nata.containers import GridArray
+        >>> from nata.containers import Axis
+        >>> import numpy as np
+        >>> x = np.arange(5)
+        >>> data = np.arange(25).reshape((5, 5))
+        >>> grid = GridArray.from_array(data, axes=[Axis(x), Axis(x)])
+        >>> grid.slice(constant=0, value=1).to_numpy()
+        array([5, 6, 7, 8, 9]) # the second column
+        >>> grid.slice(constant=1, value=1).to_numpy()
+        array([ 1,  6, 11, 16, 21]) # the second row
+        ```
     """
 
     if grid.ndim < 1:
@@ -97,44 +95,42 @@ def slice_grid_dataset(
     constant: Union[str, int],
     value: float,
 ) -> GridDataset:
-    """Takes a slice of a `GridDataset` at a constant value of a given axis.
+    """
+    Takes a slice of a `GridDataset` at a constant value of a given axis.
     Slices are not allowed along the time axis. In other words, `GridDataset`
     slices always preserve the time dependence. To do slices over the time
     axis, consider converting `grid` to a `GridArray` using the `streak()`
     plugin.
 
-    Parameters
-    ----------
-    constant: ``str`` or ``int``
-        Name or index that defines the axis taken to be constant in the slice.
-        Must not refer to the time axis.
-    comp: ``float``
-        Value of the axis at which the slice is taken.
+    Arguments:
+        constant:
+            Name or index that defines the axis taken to be constant in the slice.
+            Must not refer to the time axis.
+        value:
+            Value of the axis at which the slice is taken.
 
-    Returns
-    ------
-    :class:`nata.containers.GridDataset`:
+    Returns:
         Slice of ``grid``.
 
-    Examples
-    --------
-    Obtain a slice of a one-dimensional dataset with time dependence.
+    Examples:
+        Obtain a slice of a one-dimensional dataset with time dependence.
 
-    >>> from nata.containers import GridDataset
-    >>> from nata.containers import Axis
-    >>> import numpy as np
-    >>> time, x = np.arange(5), np.tile(np.arange(4), (5,1))
-    >>> data = np.arange(20).reshape((5, 4))
-    >>> grid = GridDataset.from_array(data, axes=[Axis(time), Axis(x)])
-    >>> grid.to_numpy()
-    array([[ 0,  1,  2,  3],
-           [ 4,  5,  6,  7],
-           [ 8,  9, 10, 11],
-           [12, 13, 14, 15],
-           [16, 17, 18, 19]])
-    >>> grid.slice(constant=1, value=0).to_numpy()
-    array([ 0,  4,  8, 12, 16]) # the first column
-
+        ```pycon
+        >>> from nata.containers import GridDataset
+        >>> from nata.containers import Axis
+        >>> import numpy as np
+        >>> time, x = np.arange(5), np.tile(np.arange(4), (5, 1))
+        >>> data = np.arange(20).reshape((5, 4))
+        >>> grid = GridDataset.from_array(data, axes=[Axis(time), Axis(x)])
+        >>> grid.to_numpy()
+        array([[ 0,  1,  2,  3],
+                [ 4,  5,  6,  7],
+                [ 8,  9, 10, 11],
+                [12, 13, 14, 15],
+                [16, 17, 18, 19]])
+        >>> grid.slice(constant=1, value=0).to_numpy()
+        array([ 0,  4,  8, 12, 16]) # the first column
+        ```
     """
 
     if grid.ndim < 2:
